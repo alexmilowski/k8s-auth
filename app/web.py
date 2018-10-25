af833f240500c36a77f99d074773274c49560b59
Alex Milowski <alex@milowski.com>
2018-10-25 10:12:13 -0700

    added use of logger for debug/warnings

--- app/web.py
+++ app/web.py
@@ -3,15 +3,14 @@ from urllib.parse import quote as uriencode
 from datetime import datetime, timedelta
 import requests
 from uuid import uuid4
-import os
-import sys
 import json
 import base64
+import logging
 
 app = Flask(__name__)
 
 def redirect_uri():
-   return current_app.config['REDIRECT_URI'] +'::authenticated::'
+   return (current_app.config['REDIRECT_URI'] if 'REDIRECT_URI' in current_app.config else request.host_url) +'::authenticated::'
 
 def set_state():
    state = str(uuid4())
@@ -23,15 +22,19 @@ def set_nonce():
    return nonce
 
 def auth_uri():
+   dest_uri = redirect_uri()
+   state = set_state()
+   nonce = set_nonce()
    return current_app.config['AUTH_PROVIDER'] + '?' + \
       'client_id=' + current_app.config['CLIENT_ID'] + \
       '&response_type=code' + \
       '&scope=openid%20email' + \
-      '&redirect_uri='+uriencode(redirect_uri()) + \
-      '&state='+set_state() + \
-      '&nonce='+set_nonce()
+      '&redirect_uri='+uriencode(dest_uri) + \
+      '&state='+state + \
+      '&nonce='+nonce
 
 def exchange_code(code):
+   logger = logging.getLogger(__name__)
    data = {
       'code' : code,
       'client_id' : current_app.config['CLIENT_ID'],
@@ -40,12 +43,18 @@ def exchange_code(code):
       'grant_type' : 'authorization_code'
    }
 
+   if logger.isEnabledFor(logging.DEBUG):
+      logger.debug('Exchanging code {code} for {redirect_uri}'.format(**data))
+
    exchange_req = requests.post(current_app.config['TOKEN_PROVIDER'],data)
 
    if exchange_req.status_code==200:
+      if logger.isEnabledFor(logging.DEBUG):
+         logger.debug('Code exchanged successfully.')
       return exchange_req.json()
    else:
-      print(exchange_req.status_code)
+      if logger.isEnabledFor(logging.DEBUG):
+         logger.debug('Cannot exchange code, {status} {response}'.format(status=exchange_req.status_code,response=exchange_req.text))
       abort(401)
 
 def get_principal(token):
@@ -57,6 +66,8 @@ def get_principal(token):
 
 @app.before_request
 def before_request():
+
+   logger = logging.getLogger(__name__)
    authenticated = 'token' in session and session['token'] is not None
 
    # check expiry
@@ -64,39 +75,45 @@ def before_request():
       if 'expiry' in session:
          elapsed = session['expiry'] - datetime.now()
          if elapsed.total_seconds()<0:
+            logger.debug('Session expired.')
             authenticated = False
             session.pop('token',None)
             session.pop('expiry',None)
       else:
          authenticated = False
+         logger.debug('No session expiry.')
          session.pop('token',None)
 
    if authenticated or request.path=='/::authenticated::':
       return
 
+   logger.debug('Not authenticated, redirecting to auth provider.')
    return redirect(auth_uri())
 
 @app.route('/::authenticated::',methods=['GET'])
 def authenticated():
-   print(session)
+   logger = logging.getLogger(__name__)
    if request.args.get('state','')!=session.get('state'):
-      print('Unauthroized, state did not match.')
+      logger.warning('Unauthroized, session "{session_state}" did not match state "{state}".'.format(session_state=session.get('state'),state=request.args.get('state','')))
       abort(401)
    session.pop('state',None)
-   print(request.args)
 
    info = exchange_code(request.args.get('code',''))
-   print(info)
 
    token = info['id_token']
+
+   if logger.isEnabledFor(logging.DEBUG):
+      logger.debug('Code exchanged for token {token}'.format(token=token))
 
    if 'WHITELIST' in current_app.config:
       principal = get_principal(token)
       if principal not in current_app.config['WHITELIST']:
-         print('Unauthorized, user not in whitelist.')
+         logger.warning('Unauthorized, {user} not in whitelist.'.format(user=principal))
          abort(401)
 
    expiry = datetime.now() + timedelta(seconds=info['expires_in'])
+   if logger.isEnabledFor(logging.DEBUG):
+      logger.debug('Token {token} expires in {expiry}'.format(token=token,expiry=expiry.isoformat()))
    session['token'] = token
    session['expiry'] = expiry
    return redirect('/')
