6eeae8d42aeb680f2cff95b95e8a2f411e9fb839
Alex Milowski <alex@milowski.com>
2018-10-25 10:09:54 -0700

    Made redirect URI optional

--- app/__main__.py
+++ app/__main__.py
@@ -8,7 +8,7 @@ from flask_session import Session
 parser = argparse.ArgumentParser(description='Authetication proxy for k8s')
 parser.add_argument('client_id',help='The client id for the authentication provider.')
 parser.add_argument('client_secret',help='The client secret for the authentication provider.')
-parser.add_argument('--redirect-uri',help='The redirect uri to use for this service.',default='http://localhost:5000/')
+parser.add_argument('--redirect-uri',help='The redirect uri to use for this service.')
 parser.add_argument('--endpoint',help='The endpoint of the service being proxied.',default='http://localhost:5000/')
 parser.add_argument('--auth-provider',help='The endpoint of the service being proxied.',default='https://accounts.google.com/o/oauth2/v2/auth')
 parser.add_argument('--token-provider',help='The endpoint of the service being proxied.',default='https://www.googleapis.com/oauth2/v4/token')
@@ -31,7 +31,8 @@ else:
 app.secret_key = args.session_key
 app.config['CLIENT_ID'] = args.client_id
 app.config['CLIENT_SECRET'] = args.client_secret
-app.config['REDIRECT_URI'] = args.redirect_uri
+if args.redirect_uri is not None:
+   app.config['REDIRECT_URI'] = args.redirect_uri
 app.config['ENDPOINT'] = args.endpoint
 app.config['AUTH_PROVIDER'] = args.auth_provider
 app.config['TOKEN_PROVIDER'] = args.token_provider
